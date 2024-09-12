5c981492157d69c3526f0a019ff7138a49ddd037
Spring-Rebirth <cjunwei6249@gmail.com>
2024-09-12 18:52:59 +0800

    实现主页的初步结构

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList, FlatListComponent } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function Home() {
     return (
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView className='bg-primary h-full'>
+            <FlatList
+                data={[{ id: 1 }, { id: 2 }]}
+                // keyExtractor={}
+                renderItem={({ item }) => {
+                    return (
+                        <Text className='text-white'>{item.id}</Text>
+
+                    )
+                }}
+                ListHeaderComponent={() => (
+                    <View className='border-2 border-red-500 h-10'>
+                        <Text className='text-white text-3xl'>Welcome Back</Text>
+                    </View>
+                )}
+            />
+
+        </SafeAreaView>
     )
 }
