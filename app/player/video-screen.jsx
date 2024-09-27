5a372f6f68d2df25d8eb6bd36226bbd993cfda93
Spring-Rebirth <cjunwei6249@gmail.com>
2024-09-28 06:43:49 +0800

    补充

--- app/player/video-screen.jsx
+++ app/player/video-screen.jsx
@@ -2,35 +2,39 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { ResizeMode, Video } from 'expo-av';
 
 function VideoScreen({ videoUrl }) {
-    <SafeAreaView className='bg-primary'>
 
-        <Video
-            source={{ uri: videoUri }}
-            useNativeControls
-            onFullscreenUpdate={handleFullscreenUpdate}
-            style={isFullscreen ? styles.fullscreenVideo : styles.normalVideo}
-        />
 
-        {isFullscreen && (
-            <Modal
-                animationType="slide"
-                transparent={true}
-                visible={isFullscreen}
-                onRequestClose={() => setIsFullscreen(false)}
-            >
-                <View style={styles.modalContainer}>
-                    <Video
-                        source={{ uri: videoUri }}
-                        useNativeControls
-                        onFullscreenUpdate={handleFullscreenUpdate}
-                        style={styles.fullscreenVideo}
-                        resizeMode="contain"
-                    />
-                </View>
-            </Modal>
-        )}
+    return (
+        <SafeAreaView className='bg-primary'>
 
-    </SafeAreaView>
+            <Video
+                source={{ uri: videoUrl }}
+                useNativeControls
+                onFullscreenUpdate={handleFullscreenUpdate}
+                style={isFullscreen ? styles.fullscreenVideo : styles.normalVideo}
+            />
+
+            {isFullscreen && (
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={isFullscreen}
+                    onRequestClose={() => setIsFullscreen(false)}
+                >
+                    <View style={styles.modalContainer}>
+                        <Video
+                            source={{ uri: videoUri }}
+                            useNativeControls
+                            onFullscreenUpdate={handleFullscreenUpdate}
+                            style={styles.fullscreenVideo}
+                            resizeMode="contain"
+                        />
+                    </View>
+                </Modal>
+            )}
+
+        </SafeAreaView>
+    )
 }
 
 export default VideoScreen;
