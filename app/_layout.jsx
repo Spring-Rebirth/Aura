6d9067276dc7792b63352dcb057717e4c22097bf
Spring-Rebirth <cjunwei6249@gmail.com>
2024-09-29 01:03:30 +0800

    实现应用内更新

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,10 @@
 import 'react-native-url-polyfill/auto';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Stack, SplashScreen } from "expo-router";
 import { useFonts } from 'expo-font';
 import { GlobalProvider } from '../context/GlobalProvider';
+import * as Updates from 'expo-updates';
+import { Alert } from 'react-native';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -19,18 +21,60 @@ export default function RootLayout() {
         "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
     });
 
+    const [isUpdating, setIsUpdating] = useState(false);
+
     useEffect(() => {
-        // 处理错误
+        // 处理字体加载错误
         if (error) throw error;
 
-        // 字体加载完成后隐藏启动屏幕
+        async function checkForUpdates() {
+            try {
+                const update = await Updates.checkForUpdateAsync();
+                if (update.isAvailable) {
+                    // 有更新可用
+                    await Updates.fetchUpdateAsync();
+                    // 通知用户，重新启动应用以应用更新
+                    Alert.alert(
+                        '有可用的更新',
+                        '已经下载了新的更新，是否立即重启应用？',
+                        [
+                            {
+                                text: '稍后',
+                                onPress: () => {
+                                    setIsUpdating(false);
+                                    SplashScreen.hideAsync(); // 隐藏启动屏幕，显示应用内容
+                                },
+                                style: 'cancel',
+                            },
+                            {
+                                text: '立即重启',
+                                onPress: async () => {
+                                    await Updates.reloadAsync();
+                                },
+                            },
+                        ],
+                        { cancelable: false }
+                    );
+                } else {
+                    // 没有更新，隐藏启动屏幕
+                    SplashScreen.hideAsync();
+                }
+            } catch (e) {
+                console.log(e);
+                // 检查更新失败，隐藏启动屏幕
+                SplashScreen.hideAsync();
+            }
+        }
+
         if (fontsLoaded) {
-            SplashScreen.hideAsync();
+            // 字体加载完成后，进行更新检查
+            checkForUpdates();
         }
 
     }, [fontsLoaded, error]);
 
-    if (!fontsLoaded && !error) {
+    if (!fontsLoaded || isUpdating) {
+        // 在字体未加载完成或正在更新时，保持启动屏幕
         return null;
     }
 
@@ -44,6 +88,5 @@ export default function RootLayout() {
                 <Stack.Screen name="search/[query]" options={{ headerShown: false }} />
             </Stack>
         </GlobalProvider>
-    )
+    );
 }
-
