8816505235b917c92e41f9283b79e332bb314062
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-03 04:30:34 +0800

    增加注释

--- context/PlayDataContext.js
+++ context/PlayDataContext.js
@@ -4,6 +4,14 @@ import React, { createContext, useRef } from 'react';
 export const PlayDataContext = createContext();
 
 export const PlayDataProvider = ({ children }) => {
+    /*
+        调用 updatePlayData 后，playDataRef.current 将如下所示：
+        {
+            abc123: { count: 7, synced: false },
+            def456: { count: 3, synced: false },
+        }
+
+    */
     const playDataRef = useRef({});
 
     const updatePlayData = (videoId, count) => {
