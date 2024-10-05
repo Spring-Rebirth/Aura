94598d4391629cb282d1fb32a74eecbd2cc1d863
Spring-Rebirth <cjunwei6249@gmail.com>
2024-10-05 20:29:25 +0800

    1

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -267,13 +267,13 @@ export default function Create() {
                     <View className='flex-row w-full justify-around'>
                         <TouchableOpacity onPress={handlePickImage}>
                             <View className='w-36 h-16 bg-[#5454eb] rounded-3xl mt-5 flex-row justify-center items-center px-4'>
-                                <Text className='text-white ml-2'>Choose file</Text>
+                                <Text className='text-white ml-2'>Choose File</Text>
                             </View>
                         </TouchableOpacity>
 
                         <TouchableOpacity onPress={generateThumbnailFromVideo}>
                             <View className='w-36 h-16 bg-[#517ae1] rounded-3xl mt-5 flex-row justify-center items-center px-4'>
-                                <Text className='text-white'>Auto generate</Text>
+                                <Text className='text-white'>Auto Generate</Text>
                             </View>
                         </TouchableOpacity>
                     </View>
